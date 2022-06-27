14de8a575e235ed7b0c094a0d31546d337393d98
skbss1224 <73568092+skbss1224@users.noreply.github.com>
2022-06-27 15:53:06 +0530

    pre and post
    pre

---
+++ pre_post.java
@@ -0,0 +1,46 @@
+package shiyam;
+
+public class pre_post {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5,b=10;
+//		System.out.println(a++);//post increment
+//		System.out.println(a);
+//		System.out.println(a++);
+//		System.out.println(a);
+//		System.out.println(++a);//pre increment
+//		System.out.println(a);
+//		System.out.println(++a);
+//		System.out.println(++a);
+//		System.out.println(b--);//post decrement
+//		System.out.println(b);
+//		System.out.println(--b);//pre decrement
+//		System.out.println(b);
+//		System.out.println(++a + a++ + ++a + ++a);
+						//(6+6+8+9)
+						//(29)
+//		System.out.println(a+++ ++a+ ++b + ++a);
+						//(5+7+11+8)
+		//					(31)
+		
+//		System.out.println(a++ + --b - ++a + a++);
+						//(5+9-7+7)
+//		System.out.println(a++ - b-- +(a+++ b-- + ++a));
+						//(5-10+(6+9+8))
+						//(5)
+//		int i=10;
+//		while(i<=10) System.out.println("blue jeans");
+//			
+//			
+//		i=i+1;
+//		System.out.println("black jeans");
+		
+//		for(;;) {
+//			System.out.println("hi");
+//		}
+		
+		
+	}
+
+}
